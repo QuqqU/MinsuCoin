8d91af95ae23c0ceac443eb3012d21e1cef66c7b
QuqqU <rldnd913@naver.com>
2018-04-18 22:40:02 +0900

    first commit

---
+++ BlockChain/Block.swift
@@ -0,0 +1,9 @@
+//
+//  Block.swift
+//  BlockChain
+//
+//  Created by 정기웅 on 2018. 4. 17..
+//  Copyright © 2018년 정기웅. All rights reserved.
+//
+
+import Foundation
---
+++ BlockChain/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  BlockChain
+//
+//  Created by 정기웅 on 2018. 4. 17..
+//  Copyright © 2018년 정기웅. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
